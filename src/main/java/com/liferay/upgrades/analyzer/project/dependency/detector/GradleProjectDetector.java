55ae3a58d7e06a86566cdeda29e993909d534fa4
Albert Gomes <albert.gomes@liferay.com>
2025-07-28 21:37:49 -0300

    ISSUE-48 Improve Regex to catch better the dependencies ISSUE-49 Create a validate project dependency method to check raw by raw that is not commented

--- src/main/java/com/liferay/upgrades/analyzer/project/dependency/detector/GradleProjectDetector.java
+++ src/main/java/com/liferay/upgrades/analyzer/project/dependency/detector/GradleProjectDetector.java
@@ -14,17 +14,23 @@ import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * @author Rafael Praxedes
+ * @author Albert Gomes Cabral
+ */
 public class GradleProjectDetector implements ProjectDetector {
 
     @Override
     public boolean matches(String fileName, Path file) {
         return fileName.equals("build.gradle")
             && Files.exists(Paths.get(file.getParent().toString(), "src"))
-            && _checkIsTheme(file);
+            && _validateDetector(file);
     }
 
     @Override
-    public void process(Path file, ProjectsDependencyGraphBuilder projectsDependencyGraphBuilder) {
+    public void process(
+        Path file, ProjectsDependencyGraphBuilder projectsDependencyGraphBuilder) {
+
         projectsDependencyGraphBuilder.addProject(
             _getProjectKey(
                 file.getParent().toUri().getPath(),
@@ -38,25 +44,13 @@ public class GradleProjectDetector implements ProjectDetector {
         Matcher matcher = _GRADLE_PROJECT_PATTERN.matcher(
             ProjectDetectorUtil.readFile(gradleFile));
 
-        while (matcher.find()) {
+        while (matcher.find() &&
+                !_validateProjectDependency(matcher.group())) {
+
             dependencies.add(_getProjectKey(matcher.group(1)));
         }
 
         return dependencies;
-    }
-
-    private boolean _checkIsTheme(Path path) {
-        if (!Files.exists(
-                Paths.get(path.getParent().toString(), "liferay-theme.json"))) {
-
-            Path lookAndFeelPath = Paths.get(
-                    path.getParent().toString(),
-                    "/src/main/webapp/WEB-INF/liferay-look-and-feel.xml");
-
-            return !lookAndFeelPath.toFile().exists();
-        }
-
-        return false;
     }
 
     private Project _getProjectKey(String path, String rawProjectName) {
@@ -71,9 +65,28 @@ public class GradleProjectDetector implements ProjectDetector {
        return ProjectDetectorUtil.getProjectKey(rawProjectName, _projectInfos);
     }
 
+    private boolean _validateDetector(Path path) {
+        if (!Files.exists(
+                Paths.get(path.getParent().toString(), "liferay-theme.json"))) {
+
+            Path lookAndFeelPath = Paths.get(
+                path.getParent().toString(),
+                "/src/main/webapp/WEB-INF/liferay-look-and-feel.xml");
+
+            return !lookAndFeelPath.toFile().exists();
+        }
+
+        return false;
+    }
+
+    private boolean _validateProjectDependency(String rawProjectName) {
+        return rawProjectName.contains("//") || rawProjectName.contains("#");
+    }
+
     private final Map<String, Project> _projectInfos = new HashMap<>();
 
     private static final Pattern _GRADLE_PROJECT_PATTERN = Pattern.compile(
-        "project.*\\(*[\"'](.*)[\"']\\)");
+        "//\\s+?compileOnly|compileInclude|implementation\\s+" +
+                "project.*\\(*[\"'](.*)[\"']\\)");
 
 }
