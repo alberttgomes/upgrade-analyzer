9e722899d60a1ca07bd2818546c47c4d394485f8
Albert Gomes <albert.gomes@liferay.com>
2025-07-28 21:40:27 -0300

    NOISSUE Create ExportOptionsConstant given is easier to replace these constants and propagate

---
+++ src/main/java/com/liferay/upgrades/analyzer/project/dependency/constant/ExportOptionsConstant.java
@@ -0,0 +1,14 @@
+package com.liferay.upgrades.analyzer.project.dependency.constant;
+
+/**
+ * @author Albert Gomes Cabral
+ */
+public class ExportOptionsConstant {
+
+    public static final String EXPORT_TYPE_DOT_GRAPH = "dot-graph";
+
+    public static final String EXPORT_TYPE_GAME_PLAN = "game-plan";
+
+    public static final String EXPORT_TYPE_STARTUP_GAME_PLAN = "startup-game-plan";
+
+}
