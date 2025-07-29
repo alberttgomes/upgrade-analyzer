513e3e829d2691f01be529d324db901ff30979f6
Albert Gomes <albert.gomes@liferay.com>
2025-07-28 21:42:35 -0300

    NOISSUE Improve ThemeProjectDetector and transform getThemeProjectKey in protected

--- src/main/java/com/liferay/upgrades/analyzer/project/dependency/detector/ThemeProjectDetector.java
+++ src/main/java/com/liferay/upgrades/analyzer/project/dependency/detector/ThemeProjectDetector.java
@@ -2,27 +2,26 @@ package com.liferay.upgrades.analyzer.project.dependency.detector;
 
 import com.liferay.upgrades.analyzer.project.dependency.graph.builder.ProjectsDependencyGraphBuilder;
 import com.liferay.upgrades.analyzer.project.dependency.model.Project;
-import org.w3c.dom.Document;
-import org.w3c.dom.NamedNodeMap;
-import org.w3c.dom.Node;
+
+import java.io.FileInputStream;
+import java.nio.file.Path;
+import java.util.Collections;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathFactory;
-import java.io.FileInputStream;
-import java.nio.file.Path;
-import java.util.Collections;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
 
 public class ThemeProjectDetector implements ProjectDetector {
+
     @Override
     public boolean matches(String fileName, Path file) {
-        if (fileName.equals("liferay-look-and-feel.xml")) {
-           return true;
-        }
-
-        return false;
+        return fileName.equals("liferay-look-and-feel.xml");
     }
 
     @Override
@@ -30,7 +29,7 @@ public class ThemeProjectDetector implements ProjectDetector {
         projectsDependencyGraphBuilder.addProject(getThemeProjectKey(file), Collections.emptySet());
     }
 
-    public Project getThemeProjectKey(Path file) {
+    protected Project getThemeProjectKey(Path file) {
         try {
             DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder builder = builderFactory.newDocumentBuilder();
@@ -51,9 +50,10 @@ public class ThemeProjectDetector implements ProjectDetector {
 
                 themeId = (themeId.endsWith("-theme") ? themeId : themeId + "-theme");
 
-                String themeRootPath = getThemeRootPath(file, themeId);
+                String themeRootPath = _getThemeRootPath(file, themeId);
 
-                String themeRootFolderName = themeRootPath.substring(themeRootPath.lastIndexOf("/") + 1);
+                String themeRootFolderName = themeRootPath.substring(
+                    themeRootPath.lastIndexOf("/") + 1);
 
                 if (themeRootFolderName.equalsIgnoreCase(themeId)) {
                     return new Project(themeRootFolderName, themeRootPath);
@@ -67,9 +67,8 @@ public class ThemeProjectDetector implements ProjectDetector {
         }
     }
 
-    private String getThemeRootPath(Path file, String themeId) {
+    private String _getThemeRootPath(Path file, String themeId) {
         Path parent = file.getParent();
-
 
         while (parent != null && parent != parent.getRoot()) {
             if (parent.getFileName().toString().toLowerCase().contains(themeId.toLowerCase())) {
@@ -90,7 +89,8 @@ public class ThemeProjectDetector implements ProjectDetector {
         }
 
         throw new RuntimeException(
-                "Theme root path not found for themeId = " + themeId + ", path = " + file.toAbsolutePath());
+            "Theme root path not found for themeId = " + themeId + ", path = "
+                    + file.toAbsolutePath());
     }
 
 }
